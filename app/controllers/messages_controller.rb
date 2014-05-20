34b9ce512e92ef65c8440ea065346061d3916d69
=-kriss-= <kriss@modelcitizen.com.au>
2014-05-20 23:02:20 +1000

    updated new ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,21 +10,21 @@ class MessagesController < ApplicationController
   def welcome
     render :json => '{
                       "class":"welcome",
-                      "html":"<h1>DOM 1 nation<\/h1><p>which dom on screen will reign supreme?<\/p><h2>CLICK ANY TWO ITEMS ON THE PAGE TO WATCH THEM BATTLE<\/h2><p>it\'s eleMENTAL!<\/p>"
+                      "html":"<div><h1>Click two parts of the page to see who will win!<\/h1><p><a href="http:\/\/divandconquer.herokuapp.com\/\">&lt;div&gt;&amp;conquer leaderboard<\/a><\/p><p><a href="http:\/\/github.com\/citizengit\">a GA WDI student project by Kriss Heimanis<\/a><\/p><\/div>"
                     }'
   end
 
   def fight
     render :json => '{
                       "class":"fight",
-                      "html":"<h1 id=\'fight\'>Place Your Bets<\/h1><table><tr><td><h1 id=\'nameOne\'><\/h1><\/td><td><h4>vs<\/h4><\/td><td><h1 id=\'nameTwo\'><\/h1><\/td><\/tr><tr><td><h1 id=\'scoreOne\'>0<\/h1><\/td><td><h6>score<\/h6><\/td><td><h1 id=\'scoreTwo\'>0<\/h1><\/td><\/tr><tr><td><h3 id=\'healthOne\'><\/h3><\/td><td><h6>current health<\/h6><\/td><td><h3 id=\'healthTwo\'><\/h3><\/td><\/tr><tr><td><h4 id=\'powerOne\'><\/h4><\/td><td><h6>current power<\/h6><\/td><td><h4 id=\'powerTwo\'><\/h4><\/td><\/tr><tr><td><h4 id=\'hitOne\'><\/h4><\/td><td><h6>this hit<\/h6><\/td><td><h4 id=\'hitTwo\'><\/h4><\/td><\/tr><\/table>"
+                      "html":"<div><h1 id=\'fight\'>Which dom object on screen will reign supreme?<\/h1><table><tr><td><h1 id=\'nameOne\'><\/h1><\/td><td><h4>vs<\/h4><\/td><td><h1 id=\'nameTwo\'><\/h1><\/td><\/tr><tr><td><h1 id=\'scoreOne\'>0<\/h1><\/td><td><h6>score<\/h6><\/td><td><h1 id=\'scoreTwo\'>0<\/h1><\/td><\/tr><tr><td><h3 id=\'healthOne\'>0<\/h3><\/td><td><h6>current health<\/h6><\/td><td><h3 id=\'healthTwo\'>0<\/h3><\/td><\/tr><tr><td><h4 id=\'powerOne\'>0<\/h4><\/td><td><h6>current power<\/h6><\/td><td><h4 id=\'powerTwo\'>0<\/h4><\/td><\/tr><tr><td><h4 id=\'hitOne\'>0<\/h4><\/td><td><h6>this hit<\/h6><\/td><td><h4 id=\'hitTwo\'>0<\/h4><\/td><\/tr><\/table><\/div>"
                     }'
   end
 
   def finish
     render :json => '{
                       "class":"finish",
-                      "html":"<h1>DOM 1 nation<\/h1><h2><\/h2><h2>it\'s eleMENTAL!<\/h2><button id=\'restart\'>play again<\/button><button>leaderboard<\/button>"
+                      "html":"<div><h1 id="playAgain">PLAY AGAIN<\/h1><p><a href=\"http:\/\/divandconquer.herokuapp.com\/\">&lt;div&gt;&amp;conquer leaderboard<\/a><\/p><p><a href=\"http:\/\/github.com\/citizengit\">a GA WDI student project by Kriss Heimanis<\/a><\/p><\/div>"
                     }'
   end
 
